69e349b93d784dff399cc66f63418724325e4214
Jessica Pansini <68460585+jspansini@users.noreply.github.com>
2022-12-14 03:05:29 -0300

    Add files via upload
    Basic c# exercises

---
+++ 01 - TradutorComandos/Exercicios/Tradutor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP1.Aula6
+{
+    public class Dicionario
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> dicionario = new();
+
+            dicionario["if"] = "se";
+            dicionario["while"] = "enquanto";
+            dicionario[">"] = "maior que";
+            dicionario["<"] = "menor que";
+            dicionario[">="] = "maior igual que";
+            dicionario["<="] = "menor igual que";
+            dicionario["=="] = "igual a";
+            dicionario["!="] = "diferente de";
+
+            Console.WriteLine("Bem-vindo ao dicionário de comandos C#");
+            Console.WriteLine("**************************************");
+            Console.WriteLine();
+            Console.WriteLine("Digite o comando C# que você quer traduzir:");
+
+            string comando = Console.ReadLine();
+
+            string[] comandos = comando.Split(" ");
+
+            bool chaveEncontrada;
+
+            string resposta = string.Empty;
+
+            foreach (string cmd in comandos)
+            {
+                chaveEncontrada = dicionario.TryGetValue(cmd, out string traducao);
+
+                if (chaveEncontrada)
+                {
+                    resposta += traducao + " ";
+                }
+                else
+                {
+                    resposta += cmd + " ";
+                }
+            }
+
+            Console.WriteLine($"A tradução do comando {comando} é {resposta.Trim()}.");
+        }
+    }
+}
---
+++ 02 - Tabuada/02 - Tabuada/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosSinqia
+{
+    public class Ex01_Tabuada
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine("*         Tabuada             *");
+            Console.WriteLine("*******************************");
+
+            bool sucesso;
+            int num;
+
+            do
+            {
+                Console.Write("Digite um número: ");
+                sucesso = int.TryParse(Console.ReadLine(), out num);
+            } while (!sucesso);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {num} = {i * num}");
+            }
+        }
+    }
+}
